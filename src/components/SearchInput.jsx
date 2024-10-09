d022017b4e116dd1e161ac75677a3c690e73edaa
LabiciIvan <ioanlabici@gmail.com>
2024-10-09 23:54:10 +0300

    usePopcorn-1: useRef hook to SearchInput component.
    - implemented focus and blur for the SearchInput component;

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useRef } from 'react';
+
 export default function SearchInput({onSetSearchInput, searchInput}) {
+
+    const searchElement = useRef(null);
+
+    useEffect(() => {
+        function callback(e) {
+            if (e.code === 'Enter') {
+                searchElement.current.focus();
+            }
+
+            if (e.code === 'Escape') {
+                searchElement.current.blur();
+                onSetSearchInput(prev => '');
+            }
+        }
+
+        document.addEventListener('keydown', callback);
+
+        return () => document.removeEventListener('keydown', callback);
+    }, []);
 
     return (
         <div className="second">
@@ -7,6 +28,7 @@ export default function SearchInput({onSetSearchInput, searchInput}) {
              placeholder="Search movies..."
              onChange={(e) => onSetSearchInput(prev => e.target.value)}
              value={searchInput}
+             ref={searchElement}
             />
         </div>
     )
